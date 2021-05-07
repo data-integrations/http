3a76f92d2d2a1f68877e978c2b908fb2c9cef836
Victor Duvert <victor.duvert.ext@veolia.com>
2021-05-07 15:56:12 +0200

    fix

--- src/main/java/io/cdap/plugin/http/source/common/http/HawkUtil.java
+++ src/main/java/io/cdap/plugin/http/source/common/http/HawkUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.cdap.plugin.http.source.common.http;
 
 import com.wealdtech.hawk.HawkClient;
@@ -6,6 +21,9 @@ import org.apache.http.entity.StringEntity;
 
 import java.net.URI;
 
+/**
+ * A class which contains utilities to make HAWK specific calls.
+ */
 public class HawkUtil {
 
     public static HawkClient createHawkClient(String authID, String authKey, HawkCredentials.Algorithm algorithm) {
@@ -26,7 +44,7 @@ public class HawkUtil {
             boolean payloadHashEnabled,
             String ext,
             String app,
-            String dlg){
+            String dlg) {
         String hash = null;
         if (payloadHashEnabled) {
             hash = Integer.toString(requestBody.hashCode());
