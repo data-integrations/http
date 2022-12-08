56786aaf40dbafdadbdbe4cdd7bd96ba21598934
Rahul Sharma <112750762+MrRahulSharma@users.noreply.github.com>
2022-12-08 14:22:48 +0530

    Creating the buffered reader only when iterating over the pages. (#96)

--- src/main/java/io/cdap/plugin/http/source/common/pagination/page/RecordPerLinePage.java
+++ src/main/java/io/cdap/plugin/http/source/common/pagination/page/RecordPerLinePage.java
@@ -29,17 +29,16 @@ import java.util.NoSuchElementException;
  * Returns every row of document as a structured record.
  */
 abstract class RecordPerLinePage extends BasePage {
-  private final BufferedReader bufferedReader;
+  private BufferedReader bufferedReader;
   protected final Schema schema;
   protected final BaseHttpSourceConfig config;
   protected boolean isLineRead;
   private String lastLine;
 
-  RecordPerLinePage(BaseHttpSourceConfig config, HttpResponse httpResponse) throws IOException {
+  RecordPerLinePage(BaseHttpSourceConfig config, HttpResponse httpResponse) {
     super(httpResponse);
     this.config = config;
     this.schema = config.getSchema();
-    this.bufferedReader = new BufferedReader(new InputStreamReader(httpResponse.getInputStream()));
   }
 
   @Override
@@ -49,11 +48,18 @@ abstract class RecordPerLinePage extends BasePage {
                                                           config.getFormat()));
   }
 
+  private BufferedReader getBufferedReader() throws IOException {
+    if (bufferedReader == null) {
+      this.bufferedReader = new BufferedReader(new InputStreamReader(httpResponse.getInputStream()));
+    }
+    return bufferedReader;
+  }
+
   @Override
   public boolean hasNext() {
     try {
       if (!isLineRead) {
-        lastLine = this.bufferedReader.readLine();
+        lastLine = this.getBufferedReader().readLine();
       }
       isLineRead = true;
       return lastLine != null;
