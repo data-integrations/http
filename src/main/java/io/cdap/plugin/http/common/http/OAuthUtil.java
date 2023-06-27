0e0bd78ca527ce3eed08fa47347221c19e0b9f6c
Rahul Sharma <112750762+MrRahulSharma@users.noreply.github.com>
2023-06-27 13:50:32 +0530

    OAuth Token refresh changes (#135)

--- src/main/java/io/cdap/plugin/http/common/http/OAuthUtil.java
+++ src/main/java/io/cdap/plugin/http/common/http/OAuthUtil.java
@@ -15,6 +15,7 @@
  */
 package io.cdap.plugin.http.common.http;
 
+import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableSet;
@@ -26,7 +27,9 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -34,24 +37,84 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-
+import java.time.Instant;
+import java.util.Date;
+import javax.annotation.Nullable;
 
 /**
  * A class which contains utilities to make OAuth2 specific calls.
  */
 public class OAuthUtil {
-  public static String getAccessTokenByRefreshToken(CloseableHttpClient httpclient, String tokenUrl, String clientId,
-                                                    String clientSecret, String refreshToken)
-    throws IOException {
 
+  /**
+   * Get Authorization header based on the config parameters provided
+   *
+   * @param config
+   * @return
+   * @throws IOException while creating the AccessToken
+   */
+  @Nullable
+  public static AccessToken getAccessToken(BaseHttpConfig config) throws IOException {
+
+    // auth check
+    AuthType authType = config.getAuthType();
+
+    // backward compatibility
+    if (config.getOauth2Enabled()) {
+      authType = AuthType.OAUTH2;
+    }
+
+    switch (authType) {
+      case SERVICE_ACCOUNT:
+        // get accessToken from service account
+        return OAuthUtil.getAccessTokenByServiceAccount(config);
+      case OAUTH2:
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+          return OAuthUtil.getAccessTokenByRefreshToken(client, config);
+        }
+    }
+    return null;
+  }
+
+  /**
+   * Returns true only if the expiration time set in the accessToken is before the current time.
+   * @param accessToken AccessToken instance
+   * @return  TRUE    if expiration time < current system time
+   *          FALSE   if the accessToken is null
+   *          FALSE   if the accessToken does not contain an expirationTime
+   */
+  public static boolean tokenExpired(AccessToken accessToken) {
+    if (accessToken != null) {
+      Date expiryTime = accessToken.getExpirationTime();
+      if (expiryTime != null) {
+        if (Date.from(Instant.now()).after(expiryTime)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Get the Access Token using the Refresh Token. The AccessToken returned has a valid expiration time if the
+   * token URL used to create the token returned a valid expires_in detail in the response.
+   *
+   * @param httpclient
+   * @param config
+   * @return
+   * @throws IOException
+   */
+  private static AccessToken getAccessTokenByRefreshToken(CloseableHttpClient httpclient,
+                                                         BaseHttpConfig config) throws IOException {
     URI uri;
     try {
-      uri = new URIBuilder(tokenUrl)
-        .setParameter("client_id", clientId)
-        .setParameter("client_secret", clientSecret)
-        .setParameter("refresh_token", refreshToken)
-        .setParameter("grant_type", "refresh_token")
-        .build();
+      uri = new URIBuilder(config.getTokenUrl())
+              .setParameter("client_id", config.getClientId())
+              .setParameter("client_secret", config.getClientSecret())
+              .setParameter("refresh_token", config.getRefreshToken())
+              .setParameter("grant_type", "refresh_token")
+              .build();
     } catch (URISyntaxException e) {
       throw new IllegalArgumentException("Failed to build token URI for OAuth2", e);
     }
@@ -60,17 +123,32 @@ public class OAuthUtil {
     CloseableHttpResponse response = httpclient.execute(httppost);
     String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");
 
-    JsonElement jsonElement = JSONUtil.toJsonObject(responseString).get("access_token");
-    if (jsonElement == null) {
-         throw new IllegalArgumentException("Access token not found");
+    JsonElement accessTokenElement = JSONUtil.toJsonObject(responseString).get("access_token");
+    if (accessTokenElement == null) {
+      throw new IllegalArgumentException("Access token not found");
     }
-    return jsonElement.getAsString();
+
+    JsonElement expiresInElement = JSONUtil.toJsonObject(responseString).get("expires_in");
+    Date expiresInDate = null;
+    if (expiresInElement != null) {
+      long expiresAtMilliseconds = System.currentTimeMillis()
+              + (long) (expiresInElement.getAsInt() * 1000) - 60000L;
+      expiresInDate = new Date(expiresAtMilliseconds);
+    }
+
+    return new AccessToken(accessTokenElement.getAsString(), expiresInDate);
   }
-  
-  public static String getAccessTokenByServiceAccount(BaseHttpConfig config) throws IOException {
-    GoogleCredentials credential;
-    String accessToken = "";
+
+  /**
+   * Get the Access Token using the Service Account details from the config object. The AccessToken returned has a
+   * valid ExpirationTime set in the returned AccessToken instance.
+   * @param config
+   * @return
+   * @throws IOException
+   */
+  private static AccessToken getAccessTokenByServiceAccount(BaseHttpConfig config) throws IOException {
     try {
+      GoogleCredentials credential;
       ImmutableSet scopeSet = ImmutableSet.of("https://www.googleapis.com/auth/cloud-platform");
       if (config.getServiceAccountScope() != null) {
         String[] scopes = config.getServiceAccountScope().split("\n");
@@ -78,24 +156,27 @@ public class OAuthUtil {
           scopeSet = ImmutableSet.builder().addAll(scopeSet).add(scope).build();
         }
       }
-      if (config.isServiceAccountJson()) {
-        InputStream jsonInputStream = new ByteArrayInputStream(config.getServiceAccountJson()
-                .getBytes(StandardCharsets.UTF_8));
-        credential = GoogleCredentials.fromStream(jsonInputStream)
-                .createScoped(scopeSet);
-      } else if (config.isServiceAccountFilePath() && !Strings.isNullOrEmpty(config.getServiceAccountFilePath())
-              && !BaseHttpSourceConfig.PROPERTY_AUTO_DETECT_VALUE.equals(config.getServiceAccountFilePath())) {
-        credential = GoogleCredentials.fromStream(new FileInputStream(config.getServiceAccountFilePath()))
-                .createScoped(scopeSet);
-      } else {
-        credential = GoogleCredentials.getApplicationDefault()
-                .createScoped(scopeSet);
-      }
-      accessToken = credential.refreshAccessToken().getTokenValue();
+      credential = getGoogleCredentials(config).createScoped(scopeSet);
+      return credential.refreshAccessToken();
     } catch (Exception e) {
-      throw new IllegalArgumentException("Failed to generate Access Token with given Service Account information", e);
+      throw new IllegalArgumentException(
+              "Failed to generate Credentials with the given Service Account information", e);
     }
-    return accessToken;
+  }
+
+  private static GoogleCredentials getGoogleCredentials(BaseHttpConfig config) throws IOException {
+    GoogleCredentials credential;
+    if (config.isServiceAccountJson()) {
+      InputStream jsonInputStream = new ByteArrayInputStream(config.getServiceAccountJson()
+              .getBytes(StandardCharsets.UTF_8));
+      credential = GoogleCredentials.fromStream(jsonInputStream);
+    } else if (config.isServiceAccountFilePath() && !Strings.isNullOrEmpty(config.getServiceAccountFilePath())
+            && !BaseHttpSourceConfig.PROPERTY_AUTO_DETECT_VALUE.equals(config.getServiceAccountFilePath())) {
+      credential = GoogleCredentials.fromStream(new FileInputStream(config.getServiceAccountFilePath()));
+    } else {
+      credential = GoogleCredentials.getApplicationDefault();
+    }
+    return credential;
   }
 }
 
