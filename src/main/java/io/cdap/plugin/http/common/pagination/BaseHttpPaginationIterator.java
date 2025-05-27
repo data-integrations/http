416154377cd6740a99fc96eef29b5332de6bbd65
vikasrathee-cs <vikas.rathee@cloudsufi.com>
2025-05-27 10:33:34 +0530

    Add handling for Null pointer exception in case of Null response

--- src/main/java/io/cdap/plugin/http/common/pagination/BaseHttpPaginationIterator.java
+++ src/main/java/io/cdap/plugin/http/common/pagination/BaseHttpPaginationIterator.java
@@ -123,7 +123,8 @@ public abstract class BaseHttpPaginationIterator implements Iterator<BasePage>, 
         break;
       case STOP:
         throw new IllegalStateException(String.format("Fetching from url '%s' returned status code '%d' and body '%s'",
-                                                      nextPageUrl, httpStatusCode, response.getBody()));
+                                                      nextPageUrl, httpStatusCode,
+                                                      response == null ? null : response.getBody()));
       case SKIP:
       case SEND:
         if (!this.supportsSkippingPages()) {
@@ -132,7 +133,7 @@ public abstract class BaseHttpPaginationIterator implements Iterator<BasePage>, 
             config.getPaginationType()));
         }
         LOG.warn(String.format("Fetching from url '%s' returned status code '%d' and body '%s'",
-                               nextPageUrl, httpStatusCode, response.getBody()));
+                               nextPageUrl, httpStatusCode, response == null ? null : response.getBody()));
         // this will be handled by PageFactory. Here no handling is needed.
         break;
       default:
